2d4ce1bff2c03bb094e58f26af6d23444cc14898
Denise <meagher.denise@gmail.com>
2016-08-13 19:13:17 -0400

    make uber model

--- mobilecare/db/schema.rb
+++ mobilecare/db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160813200633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ubers", force: :cascade do |t|
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
