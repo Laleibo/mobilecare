e67f54bb8b4fdcc8fc6d9302c81cd93beaf66ae7
Zach Roberts <zach.ker.roberts@gmail.com>
2016-08-13 15:05:09 -0400

    created Ride model

--- mobilecare/db/schema.rb
+++ mobilecare/db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160813190427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rides", force: :cascade do |t|
+    t.integer  "appointment_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
 end
