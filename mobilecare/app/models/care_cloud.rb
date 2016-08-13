12e2299592f22ef44eec4bd80e9057e1a039b248
Zach Roberts <zach.ker.roberts@gmail.com>
2016-08-13 19:07:32 -0400

    added geolocator and converted addresses to long latt

--- mobilecare/app/models/care_cloud.rb
+++ mobilecare/app/models/care_cloud.rb
@@ -1,9 +1,7 @@
 class CareCloud < ApplicationRecord
-
   def self.get_appointment
     header = {'Authorization' => 'Bearer Vsk0x3pTxSDnSJRds5tOyPEC5ehAx7g4'}
      appointment = HTTParty.get('https://external-api-gateway.development.carecloud.com/v2/appointments?start_date=20160801&end_date=20160830', headers: header)
-
   end
   def self.get_appointment_id
       CareCloud.get_appointment[0]["appointment"]["id"]
@@ -20,10 +18,11 @@ class CareCloud < ApplicationRecord
     CareCloud.get_patient["patient"]["addresses"].each do |address|
       if address["is_primary"] == true
         @street_address = address["line1"]
-        @zip_code = address["zip"]
+        @city = address["city"]
+        @state = address["state"]
       end
     end
-    Geocoder.coordinates
+    Geocoder.coordinates(@street_address + @city + @state)
   end
   def self.get_time_of_appointment
     CareCloud.get_appointment[0]["appointment"]["start_time"]
@@ -34,11 +33,11 @@ class CareCloud < ApplicationRecord
     locations = HTTParty.get('https://external-api-gateway.development.carecloud.com/v2/locations', headers: header)["locations"]
     locations.each do |location|
       if location["id"] = location_id
-        @office_zip_code = location["address"]["zip_code"]
         @office_street_address = location["address"]["line1"]
+        @office_street_city = location["address"]["city"]
+        @office_state = location["address"]["state_name"]
       end
     end
-    @office_street_address
+      Geocoder.coordinates(@office_street_address + @office_street_city + @office_state)
   end
-
 end
