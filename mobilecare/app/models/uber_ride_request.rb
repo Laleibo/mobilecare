193c1f68e89504d23f25c5aa36973e677b2c0f16
Zach Roberts <zach.ker.roberts@gmail.com>
2016-08-14 08:25:55 -0400

    lyft

--- mobilecare/app/models/uber_ride_request.rb
+++ mobilecare/app/models/uber_ride_request.rb
@@ -1,50 +1,4 @@
 class UberRideRequest < ApplicationRecord
-  def self.create_ride
-    start_lat = CareCloud.get_patient_address[0]
-    start_long = CareCloud.get_patient_address[1]
-    end_lat = CareCloud.get_office_location[0]
-    end_long = CareCloud.get_office_location[1]
-    header = {'Authorization' => "Bearer #{ENV['UBER_AUTH']}"}
-    body = {"scope" => "request"}
-    HTTParty.post("https://sandbox-api.uber.com/v1/requests", headers: header, body: body.to_json)
-  #   {
-  #  request_id: "852b8fdd-4369-4659-9628-e122662ad257",
-  #  product_id: "a1111c8c-c720-46c3-8534-2fcdd730040d",
-  #  status: "processing",
-  #  vehicle: "Lamborgni Mercy",
-  #  driver: "Ronald Mcdonald",
-  #  location: "Mars",
-  #  eta: 5,
-  #  surge_multiplier: 1.0
-  #     }
-  end
-
-  def self.ride_request
-    start_lat = CareCloud.get_patient_address[0]
-    start_long = CareCloud.get_patient_address[1]
-    end_lat = CareCloud.get_office_location[0]
-    end_long = CareCloud.get_office_location[1]
-
-    header = {'Authorization' => "Bearer #{ENV['UBER_AUTH']}"}
-    HTTParty.get("https://sandbox-api.uber.com/v1/estimates/price?start_latitude=#{start_lat}&start_longitude=#{start_long}&end_latitude=#{end_lat}&end_longitude=#{end_long}", headers: header, body: body.to_json)
-  end
-
-  def self.pick_up_time
-    ride_time_in_minutes = UberRideRequest.time_estimation / 60.0
-    appointment_time = DateTime.strptime(CareCloud.get_time_of_appointment)
-    app_time_in_secs = (appointment_time.hour * (60 * 60)) + (appointment_time.minute * (60))
-    seconds_uber_pick_up = app_time_in_secs - 900 - (ride_time_in_minutes * 60)
-    app_hour = seconds_uber_pick_up.to_i / (60 * 60)
-    app_min = (seconds_uber_pick_up.to_i % (60 * 60)) / 60
-  end
-
-  def self.schedule_ride
-    Ride.create!(appointment_id: CareCloud.get_appointment_id, wants_ride: true, confirm_ride: false, cancel_ride: false, lattitude: CareCloud.get_patient_address[0], longitude: CareCloud.get_patient_address[1], request_id:'852b8fdd-4369-4659-9628-e122662ad257', price_estimation: UberRideRequest.price_estimation, pick_up_time: pick_up_time, eta: 0, note_to_driver: "0", return_ride: true)
-  end
-
-  def self.cancel_ride
-    @ride.destroy if @ride.cancel_ride == true
-  end
 
   def self.time_estimation
     UberRideRequest.ride_request["prices"][1]["duration"]
@@ -54,20 +8,60 @@ class UberRideRequest < ApplicationRecord
     UberRideRequest.ride_request["prices"][1]["low_estimate"]
   end
 
-  def self.ride_reminder
-    event = Event.new
-    body = {reminder_time: (Time.now + 5.minutes).to_i, phone_number: "+4511241342", event: event.as_json, time: event.time}
-      header = {'Authorization' => 'Token vhKRfrggbm7HPxanf4RfQnXf_i3dIAD_8ISj4IyL', 'Content-Type' => 'application/json'}
-    HTTParty.post("https://sandbox-api.uber.com/v1/reminders", headers: header, body: body.to_json )
+  def self.create_ride
+    start_lat = CareCloud.get_patient_address[0]
+    start_long = CareCloud.get_patient_address[1]
+    end_lat = CareCloud.get_office_location[0]
+    end_long = CareCloud.get_office_location[1]
+    header = {'Authorization' => "Bearer #{ENV['UBER_AUTH']}"}
+    body = {"scope" => "request"}
+    HTTParty.post("https://sandbox-api.uber.com/v1/requests", headers: header, body: body.to_json)
   end
 
-  # def get_eta
-  #   #ride_id = can't get it yet :)
-  #   header = {'Authorization' => 'Token vhKRfrggbm7HPxanf4RfQnXf_i3dIAD_8ISj4IyL', 'Content-Type' => 'application/json'}
-  # HTTParty.post("https://sandbox-api.uber.com//v1/requests/#{ride_id}", headers: header,
-  # destination.eta
-  # end
+  def self.ride_request
+    start_lat = CareCloud.get_patient_address[0]
+    start_long = CareCloud.get_patient_address[1]
+    end_lat = CareCloud.get_office_location[0]
+    end_long = CareCloud.get_office_location[1]
+    header = {'Authorization' => "Bearer #{ENV['UBER_AUTH']}"}
+    HTTParty.get("https://sandbox-api.uber.com/v1/estimates/price?start_latitude=#{start_lat}&start_longitude=#{start_long}&end_latitude=#{end_lat}&end_longitude=#{end_long}", headers: header, body: body.to_json)
+    end
+    def self.pick_up_time
+      ride_time_in_minutes = UberRideRequest.time_estimation / 60.0
+      appointment_time = DateTime.strptime(CareCloud.get_time_of_appointment)
+      app_time_in_secs = (appointment_time.hour * (60 * 60)) + (appointment_time.minute * (60))
+      seconds_uber_pick_up = app_time_in_secs - 900 - (ride_time_in_minutes * 60)
+      app_hour = seconds_uber_pick_up.to_i / (60 * 60)
+      app_min = (seconds_uber_pick_up.to_i % (60 * 60)) / 60
+    end
+    def self.lyft_ride
+      start_lat = CareCloud.get_patient_address[0]
+      start_long = CareCloud.get_patient_address[1]
+      end_lat = CareCloud.get_office_location[0]
+      end_long = CareCloud.get_office_location[1]
+      body = {"ride_type" => "lyft", "origin" => {"lat" => start_lat, "lng" => start_long }, "destination" => {"lat" => end_lat, "lng" => end_long} }
+      header = {'Authorization' => ' Bearer SANDBOX-gAAAAABXsFatBDofKkj4i6p8UjtkUCx7R2_nVBxDSSoqzejOQaUVvkTplQH0q9shIjq356aiow0NZhOo8IUiWGEDANuY31ujxryBxE8d7e-7LyhmHY-HWHxKYz_LZbFBjpms1iPqG33lbY2dxfuiEee9tmnXhHet8H57Y8bvVXLvxgG6K_Ig96gDGeijiLWps2mFiHLOU0MBgjg9Gs5I0sUfGH8HL7Bkt_a4PHUTYenpRhDR5bCGcgxLTCz3rr8ePUO92f5uNgYX', 'Content-Type' => 'application/json'}
+      HTTParty.post("https://api.lyft.com/v1/rides", headers: header, body: body.to_json )
+    end
+    def self.simulate_driver_arrived
+      ride_id = UberRideRequest.simluate_driver_accepted["ride_id"]
+      body = {"status" => "arrived"}
+      header = {'Authorization' => ' Bearer SANDBOX-gAAAAABXsFatBDofKkj4i6p8UjtkUCx7R2_nVBxDSSoqzejOQaUVvkTplQH0q9shIjq356aiow0NZhOo8IUiWGEDANuY31ujxryBxE8d7e-7LyhmHY-HWHxKYz_LZbFBjpms1iPqG33lbY2dxfuiEee9tmnXhHet8H57Y8bvVXLvxgG6K_Ig96gDGeijiLWps2mFiHLOU0MBgjg9Gs5I0sUfGH8HL7Bkt_a4PHUTYenpRhDR5bCGcgxLTCz3rr8ePUO92f5uNgYX'}
 
+      HTTParty.put("https://api.lyft.com/v1/sandbox/rides/" + ride_id, headers: header, body: body)
+    end
+    def self.simulate_driver_accepted
+      ride_id = UberRideRequest.lyft_ride["ride_id"]
+      body = {"status" => "accepted"}
+      header = {'Authorization' => ' Bearer SANDBOX-gAAAAABXsFatBDofKkj4i6p8UjtkUCx7R2_nVBxDSSoqzejOQaUVvkTplQH0q9shIjq356aiow0NZhOo8IUiWGEDANuY31ujxryBxE8d7e-7LyhmHY-HWHxKYz_LZbFBjpms1iPqG33lbY2dxfuiEee9tmnXhHet8H57Y8bvVXLvxgG6K_Ig96gDGeijiLWps2mFiHLOU0MBgjg9Gs5I0sUfGH8HL7Bkt_a4PHUTYenpRhDR5bCGcgxLTCz3rr8ePUO92f5uNgYX'}
+
+      HTTParty.put("https://api.lyft.com/v1/sandbox/rides/" + ride_id, headers: header, body: body)
+    end
+  def self.get_status
+    body = {"status" => "accepted"}
+    header = {'Authorization' => ' Bearer SANDBOX-gAAAAABXsFatBDofKkj4i6p8UjtkUCx7R2_nVBxDSSoqzejOQaUVvkTplQH0q9shIjq356aiow0NZhOo8IUiWGEDANuY31ujxryBxE8d7e-7LyhmHY-HWHxKYz_LZbFBjpms1iPqG33lbY2dxfuiEee9tmnXhHet8H57Y8bvVXLvxgG6K_Ig96gDGeijiLWps2mFiHLOU0MBgjg9Gs5I0sUfGH8HL7Bkt_a4PHUTYenpRhDR5bCGcgxLTCz3rr8ePUO92f5uNgYX'}
+    HTTParty.put("https://api.lyft.com/v1/sandbox/rides/" + ride_id, headers: header, body: body)
+  end
 
   def self.pick_up_time
     ride_time_in_minutes = UberRideRequest.time_estimation / 60.0
@@ -77,27 +71,18 @@ class UberRideRequest < ApplicationRecord
     app_hour = seconds_uber_pick_up.to_i / (60 * 60)
     app_min = (seconds_uber_pick_up.to_i % (60 * 60)) / 60
   end
-  def self.lyft_ride
-
-    header = {'Authorization' => ' Bearer SANDBOX-gAAAAABXsEM-nuXlcwrHMNIQkxh9ulGw2lOrcI3Y9A66wJd-xJne0FluFaa-P7nxaAdrTbVSuyaXsenRAeo3Hem69vWh01YJdSrnJ5L4uUEPFSvHBFebJ3zzww9XHd6Mu35hz7TAeBJYn-RXE8x-pRZhZ_wRnmgclu4hVuZfw0YdxC1VikkQbJL0oDIR4H6v3sLFD_nK6rI3rK4znn9i24F0-0j7SzCT4D_t6_eKw-ttGUmLLlrAd7ZYFbmYAg7DIIztz4_IEpsa', 'Content-Type' => 'application/json'}
-  HTTParty.post("https://api.lyft.com/v1/rides", headers: header, body: body )
-  end
 end
-
-
-
-
-
 
 class Event
   attr_accessor :time, :name
   def initialize
     @time = (Time.now + 5.minutes).to_i
     @name = "Rick James"
-
   end
   def as_json
     {time: @time, name: @name}
   end
 
 end
+
+# curl --include -X POST -u 'sUy_984mrKth:SANDBOX-_D-v3z2dsDmFVl61HKIRSj5IJ0r-1Xn2' -H 'Content-Type: application/json;charset=UTF-8' --data '{"grant_type": "client_credentials", "expires_in": 100000000000, "scope": "rides.request"}' 'https://api.lyft.com/oauth/token'
