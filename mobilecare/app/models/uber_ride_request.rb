5e2654db2af0c3252e7f6ec6a05eca2f0a6806a9
Zach Roberts <zach.ker.roberts@gmail.com>
2016-08-14 06:52:12 -0400

    d

--- mobilecare/app/models/uber_ride_request.rb
+++ mobilecare/app/models/uber_ride_request.rb
@@ -5,18 +5,18 @@ class UberRideRequest < ApplicationRecord
     end_lat = CareCloud.get_office_location[0]
     end_long = CareCloud.get_office_location[1]
     header = {'Authorization' => "Bearer #{ENV['UBER_AUTH']}"}
-
-    # HTTParty.get("https://sandbox-api.uber.com/v1/requests/8abd8aeb-87ff-433b-9dd7-57f7fe922aff", headers: header)
-    {
-   request_id: "852b8fdd-4369-4659-9628-e122662ad257",
-   product_id: "a1111c8c-c720-46c3-8534-2fcdd730040d",
-   status: "processing",
-   vehicle: "Lamborgni Mercy",
-   driver: "Ronald Mcdonald",
-   location: "Mars",
-   eta: 5,
-   surge_multiplier: 1.0
-      }
+    body = {"scope" => "request"}
+    HTTParty.post("https://sandbox-api.uber.com/v1/requests", headers: header, body: body.to_json)
+  #   {
+  #  request_id: "852b8fdd-4369-4659-9628-e122662ad257",
+  #  product_id: "a1111c8c-c720-46c3-8534-2fcdd730040d",
+  #  status: "processing",
+  #  vehicle: "Lamborgni Mercy",
+  #  driver: "Ronald Mcdonald",
+  #  location: "Mars",
+  #  eta: 5,
+  #  surge_multiplier: 1.0
+  #     }
   end
 
   def self.ride_request
@@ -24,8 +24,9 @@ class UberRideRequest < ApplicationRecord
     start_long = CareCloud.get_patient_address[1]
     end_lat = CareCloud.get_office_location[0]
     end_long = CareCloud.get_office_location[1]
+
     header = {'Authorization' => "Bearer #{ENV['UBER_AUTH']}"}
-    HTTParty.get("https://sandbox-api.uber.com/v1/estimates/price?start_latitude=#{start_lat}&start_longitude=#{start_long}&end_latitude=#{end_lat}&end_longitude=#{end_long}", headers: header)
+    HTTParty.get("https://sandbox-api.uber.com/v1/estimates/price?start_latitude=#{start_lat}&start_longitude=#{start_long}&end_latitude=#{end_lat}&end_longitude=#{end_long}", headers: header, body: body.to_json)
   end
 
   def self.pick_up_time
@@ -77,7 +78,7 @@ class UberRideRequest < ApplicationRecord
     app_min = (seconds_uber_pick_up.to_i % (60 * 60)) / 60
   end
   def self.lyft_ride
-    
+
     header = {'Authorization' => ' Bearer SANDBOX-gAAAAABXsEM-nuXlcwrHMNIQkxh9ulGw2lOrcI3Y9A66wJd-xJne0FluFaa-P7nxaAdrTbVSuyaXsenRAeo3Hem69vWh01YJdSrnJ5L4uUEPFSvHBFebJ3zzww9XHd6Mu35hz7TAeBJYn-RXE8x-pRZhZ_wRnmgclu4hVuZfw0YdxC1VikkQbJL0oDIR4H6v3sLFD_nK6rI3rK4znn9i24F0-0j7SzCT4D_t6_eKw-ttGUmLLlrAd7ZYFbmYAg7DIIztz4_IEpsa', 'Content-Type' => 'application/json'}
   HTTParty.post("https://api.lyft.com/v1/rides", headers: header, body: body )
   end
