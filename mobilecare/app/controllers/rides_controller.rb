c18b4d1e348f33e2f7c6026eeb61de36aa06af5e
Laleibo <Laleibo@gmail.com>
2016-08-14 03:14:16 -0400

    need the pick_up_time method

--- mobilecare/app/controllers/rides_controller.rb
+++ mobilecare/app/controllers/rides_controller.rb
@@ -1,4 +1,5 @@
 class RidesController < ApplicationController
+  before_action :set_request_id only:, [:update]
   def new
     @ride = Ride.new()
   end
@@ -18,11 +19,9 @@ class RidesController < ApplicationController
   end
 
   def update
+    
   end
 
-  def update_time
-    @ride = Ride.find_by()
-  end
 
 
 private
@@ -31,5 +30,8 @@ def ride_params
   params.require(:appointment_id, :wants_ride, :lattitude, :longitude, :return_ride).permit(:confirm_ride, :cancel_ride, :pick_up_time, :eta, :note_to_driver, :request_id, :price_estimation)
 end
 
+def set_request_id
+  @ride= Ride.find_by(request_id: ride_params[:request_id])
+end
 
 end
